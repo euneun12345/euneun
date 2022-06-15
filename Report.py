3e4ec82194de0c7a0425bd9b0351376f4c06ef5f
euneun12345 <104140018+euneun12345@users.noreply.github.com>
2022-06-15 18:20:01 +0900

    Create Report.py

---
+++ Report.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import paho.mqtt.client as mqtt
+
+led = 4
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led, GPIO.OUT)
+
+client = mqtt.Client()
+client.connect("119.200.109.51", 1883, 60)
+client.loop_start()
+client.subscribe("led")
+
+def on_message(client, userdata, message):
+   if str(message.payload.decode("utf-8")) == "1":
+       GPIO.output(led, 1)
+
+   else:
+       GPIO.output(led, 0)
+
+while True:
+   client.on_message = on_message
+
+GPIO.cleanup()
